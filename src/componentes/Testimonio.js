3d9bf86d3565543105bfc1391d6cdd2873ac74e5
Mauricio Basalo <mbasalogutierrez@gmail.com>
2023-08-07 17:47:27 -0300

    scnd commit

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import '../hojas-de-estilo/Testimonio.css'
+
+function Testimonio(props) {
+  return (
+    <div className='contenedor-testimonio'>
+
+      <img 
+        className='imagen-testimonio'
+        src={require('../imagenes/testimonio-emma.png')}
+        alt='Foto de Emma'
+      />
+
+      <div className='contenedor-texto-testimonio'> 
+        <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
+      </div>  
+    
+    </div>
+
+  );
+}
+
+export default Testimonio;
